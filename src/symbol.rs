455122227d65c97397c0d7087e876bf408bf0a61
mrrogge <5900418+mrrogge@users.noreply.github.com>
2024-05-26 20:36:32 -0500

    add displays for symbol kinds

--- src/symbol.rs
+++ src/symbol.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use std::collections::HashMap;
 
 use crate::ast::{AstFnDecl, SpkType, AST};
@@ -10,4 +11,15 @@ pub enum SymbolKind {
     Type(SpkType),
     FunctionDef(AstFnDecl),
     Task(AST),
+}
+
+impl fmt::Display for SymbolKind {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SymbolKind::Var(type_) => write!(f, "tag:{}", type_),
+            SymbolKind::Type(type_) => write!(f, "type:{}", type_),
+            SymbolKind::FunctionDef(decl) => write!(f, "fnDef:{}", decl.id),
+            SymbolKind::Task(_) => write!(f, "task"),
+        }
+    }
 }
